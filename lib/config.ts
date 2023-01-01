711f4d0a8d14751b07e7795f700b993da5c42798
Morgan <14006067+hellofrommorgan@users.noreply.github.com>
2023-01-01 10:50:09 -0800

    maybe fixes it

--- lib/config.ts
+++ lib/config.ts
@@ -1,8 +1,8 @@
 // TODO: change these to your own values
 // NOTE: rootNotionSpaceId is optional; set it to undefined if you don't want to
 // use it.
-export const rootNotionPageId = '067dd719a912471ea9a3ac10710e7fdf'
-export const rootNotionSpaceId = 'fde5ac74-eea3-4527-8f00-4482710e1af3'
+export const rootNotionPageId = '87539e0219014b3993b9438395011d90'
+export const rootNotionSpaceId = ''
 
 // NOTE: having this enabled can be pretty expensive as it re-generates preview
 // images each time a page is built. In a production setting, we recommend that
