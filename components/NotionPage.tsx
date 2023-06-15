b9f387900a39ae14ae67779cf4aac4ca85f4edd3
Morgan <14006067+hellofrommorgan@users.noreply.github.com>
2023-06-15 07:54:39 -0700

    Update NotionPage.tsx

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -22,6 +22,24 @@ const Code = dynamic(() =>
   import('react-notion-x/build/third-party/code').then((m) => m.Code)
 );
 
+const Equation = dynamic(() =>
+  import('react-notion-x/build/third-party/equation').then((m) => m.Equation)
+);
+
+const Pdf = dynamic(
+  () => import('react-notion-x/build/third-party/pdf').then((m) => m.Pdf),
+  {
+    ssr: false
+  }
+);
+
+const Modal = dynamic(
+  () => import('react-notion-x/build/third-party/modal').then((m) => m.Modal),
+  {
+    ssr: false
+  }
+);
+
 export const NotionPage = ({
   recordMap: notionRecordMap,
   previewImagesEnabled,
